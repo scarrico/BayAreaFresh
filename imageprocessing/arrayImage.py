3e90fb792afa63af0ee56f22a9bb0ac53037a935
Sandra Carrico <scarrico@users.noreply.github.com>
2017-06-16 19:04:03 -0700

    Now whitelists allowed characters from tesseract
    Restricts characters that OCR can return to allowed character set: a-Z0-9-.  Flake-8 also run.

--- imageprocessing/arrayImage.py
+++ imageprocessing/arrayImage.py
@@ -27,33 +27,42 @@ class arrayImage(object):
         self.rightMargin = int(round(750*self.scale))
         self.rightMarginWords = int(round(240*self.scale))
         self.imageLoc = ""
-
+        # whiteList = list(ascii_letters + digits)
+        self.whiteList = ""
+        gen = (i for j in (range(ord('a'), ord('z')+1),
+                           range(ord('A'), ord('Z')+1),
+                           range(ord('0'), ord('9')+1)) for i in j)
+        
+        for i in gen:
+            self.whiteList = self.whiteList+chr(i)
+        self.whiteList = self.whiteList+chr(ord('-'))
+        
     def readArray(self, loc, printFile="imgReadIn.png"):
         self.imageLoc = loc
         self.image = cv2.imread(loc)
         return self.image
-
+                   
     def image2Gray(self, img, printFile="grayTransform.png"):
         self.grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return self.grayImage
-
+                   
     def cropArray(self, img):
         return img[self.topMargin4Array:self.bottomMargin4Array,
                    self.leftMargin:self.rightMargin]
-
+                   
     def cropDateInfo(self, img):
         return img[self.topMargin4Date:self.bottomMargin4Date,
                    self.leftMargin:self.rightMargin]
-
+                                    
     def cropWords(self, img):
         return img[self.topMargin4Array:self.bottomMargin4Array,
                    self.leftMargin:self.rightMarginWords]
-
+        
     def segmentWithThreshold(self, img):
         ret, thresh = cv2.threshold(img, 0, 255,
                                     cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
         return thresh
-
+        
     def cleanImage(self, img):
         # INTER_LANCZOS4 is Similar to INTER_CUBIC
         # Magnify
@@ -86,6 +95,8 @@ class arrayImage(object):
         # Read off the labels one at a time.  Need a margin
         # since otherwise the cropping is too close for tesseract.
         with PyTessBaseAPI() as api:
+            # api.Init(".","eng",tesseract.OEM_DEFAULT)
+            api.SetVariable("tessedit_char_whitelist", self.whiteList)
             api.SetImage(img)
             boxes = api.GetComponentImages(RIL.TEXTLINE, True)
             for i, (im, box, _, _) in enumerate(boxes):
@@ -95,18 +106,20 @@ class arrayImage(object):
                 croppedSegment = sharp[box['y']:box['y']+box['h']+margin,
                                        box['x']:box['x']+box['w']+margin]
                 ocrResult = api.GetUTF8Text()
+                # Mean confidences
                 conf = api.MeanTextConf()
+                print("confidences: ", api.AllWordConfidences())
                 print(ocrResult)
                 tailPrint = "\n"+ocrResult+"end image"
                 logger.debug(VisualRecord("ocrResult",
                                           croppedSegment, tailPrint))
-                print(repr(box)) 
-        
+                print(repr(box))
+
     # Produces outline of bars
     def segmentWithCanny(self, img):
         edges = cv2.Canny(img, 100, 200)
         return edges
-        
+
     @property
     def image(self):
         return self._image
@@ -114,15 +127,15 @@ class arrayImage(object):
     @image.setter
     def image(self, value):
         self._image = value
-        
+
     @property
-    def grayImage(self): 
+    def grayImage(self):
         return self._grayImage
-        
+
     @grayImage.setter
     def grayImage(self, value):
         self._grayImage = value
-                   
+
 
 # BEGIN Set up debugging system
 logger = logging.getLogger("demo")
@@ -130,11 +143,13 @@ fh = FileHandler('test.html', mode="w")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 # END Set up debugging system
-                   
+
 arrayDaily = arrayImage()
 imageLoc = "../arrays/daily/Dow/array/daily-Dow-array-2017-05-30-20.27.08.png"
 img = arrayDaily.readArray(imageLoc)
-im_bw = arrayDaily.cleanImage(img)  
+im_bw = arrayDaily.cleanImage(img)
 sharp = arrayDaily.cropArray(im_bw)
+arrayDaily.OcrSegment(sharp)
+exit()
 sharp = arrayDaily.cropWords(im_bw)
 arrayDaily.OcrSegment(sharp)
